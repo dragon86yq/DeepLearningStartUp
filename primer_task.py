2749c42e1ffc6bafb8cd1e45939d77551743a396
dragon86yq <dragon86yq@gmail.com>
2017-10-01 23:44:29 +0800

    basic task

---
+++ primer_task.py
@@ -0,0 +1,17 @@
+from collections import Counter
+words = []
+content = []
+with open('happiness_seg.txt', 'r', encoding='utf-8') as f:
+    for line in f:
+        line = line.strip()
+        if len(line) > 0:
+            words = words + line.split(' ')
+
+for i in range(len(words)):
+    if len(words[i]) == 2 and len(words[i+1]) == 2:
+        content.append(words[i] + ' ' + words[i+1])
+
+counter = Counter(content)
+counter.most_common(10)
+for a in counter.most_common(10):
+    print(a[0] + '\t' + str(a[1]))
