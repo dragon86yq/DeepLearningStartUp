1fb9f3cb03380fac4949903ae057a880fb0e41d6
dragon86yq <dragon86yq@gmail.com>
2017-10-06 09:47:13 +0800

    修改代码

--- primer_task.py
+++ primer_task.py
@@ -7,12 +7,19 @@ with open('happiness_seg.txt', 'r', encoding='utf-8') as f:
         if len(line) > 0:
             words = words + line.split(' ')
 
+# punctuation = [u'。', u'，', u'“', u'”', u'…', u'？', u'！', u'、', u'；', u'（', 
+#                         u'）',u'?',u'：', u'― ―']
+# stopwords  = [u'的', u'地', u'得', u'而', u'了', u'在', u'是', u'我', u'和', u'们', u'被', u'对', u'来说', u'它',
+#                         u'就',  u'不',  u'都',  u'上', u'也', u'很', u'到', u'他', u'要', u'更', u'有', u'一个', u'― ―'
+#                         u'去', u'你',  u'会', u'着', u'看', u'好', u'这', u'时', u'中', u'并', u'下', u'使']
+
 # 当前后两个list元素长度为2时，将其存入新建的list
-for i in range(len(words)):
-    if len(words[i]) == 2 and len(words[i+1]) == 2:
-        content.append(words[i] + ' ' + words[i+1])
+for i in range(len(words) - 1):
+    # if (words[i] not in punctuation and words[i] not in stopwords)  and (words[i+1] not in punctuation and words[i+1] not in stopwords):
+    if (len(words[i]) > 1  and len(words[i+1]) > 1):
+            content.append(words[i] + ' ' + words[i+1])
 
 counter = Counter(content)
-counter.most_common(10)
+b = counter.most_common(10)
 for a in counter.most_common(10):
     print(a[0] + '\t' + str(a[1]))
--- primer_task2.py
+++ primer_task2.py
@@ -3,10 +3,15 @@ import numpy as np
 mu = 0 # 均值
 sigma = 0.5 #标准差
 rarray = np.random.normal(mu,sigma,100)
+rarray[rarray > 0] = 0
+print(rarray)
 
+
+'''
 for i in range(len(rarray)):
     if(rarray[i] < 0):
         print(rarray[i])
         rarray[i] = 0
     else
         rarray[i] = rarray[i]
+'''
